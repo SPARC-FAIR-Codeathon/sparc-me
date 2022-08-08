24264a6409a1195a470bf63e9b91ba4369333112
Chinchien Lin <lin810116@gmail.com>
2022-08-09 07:41:12 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sparc_me",
+    version="0.0.2",
+    author="Thiranja Prasad Babarenda Gamage, Chinchien Lin, Savindi Wijenayaka, Michael Hoffman, Linkun Gao, Haribalan Kumar",
+    email="psam012@aucklanduni.ac.nz, clin864@aucklanduni.ac.nz",
+    license="Apache-2.0",
+    packages=find_packages()
+)
