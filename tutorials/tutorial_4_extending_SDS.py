890f34d2c5dee3d7bb8be8321a8ca0eb7dcfd4ba
Chinchien Lin <lin810116@gmail.com>
2022-08-08 18:42:43 +0800

    Refactor tutorial_4_extending_SDS.py

--- tutorials/tutorial_4_extending_SDS.py
+++ tutorials/tutorial_4_extending_SDS.py
@@ -3,25 +3,28 @@ This tutorial will walk you through the steps of
 extending a field to a existing metadata
 and updating the metadata schema accordingly
 """
+from pathlib import Path
+
 from sparc_me import Dataset, Schema
 
 if __name__ == '__main__':
+    save_dir = Path("./tmp")
     # Create a SDS dataset from template
     dataset = Dataset()
     dataset.load_from_template(version="1.2.3")
-    dataset.save(save_dir="./tmp/template/")
+    dataset.save(save_dir=save_dir.joinpath("template"))
 
     # Save the default schema
     default_schema = Schema.get_default_schema(version="1.2.3", category="dataset_description")
     schema = Schema()
     schema.set_schema(default_schema)
-    schema.save(save_dir="./tmp/", category="dataset_description")
+    schema.save(save_dir=save_dir.joinpath("schema"), category="dataset_description")
     #
     # Extend metadata - adding a new field
     category = "dataset_description"
     element = "consent_code"
     dataset.add_element(category=category, element=element)
-    dataset.save(save_dir="./tmp/template/")
+    dataset.save(save_dir=save_dir.joinpath("template"))
     # Update schema
     property = {
         "type": "string",
@@ -30,7 +33,7 @@ if __name__ == '__main__':
         "Example": "test"
     }
     schema.add_property(property_name=element, property=property)
-    schema.save(save_dir="./tmp/", category="dataset_description")
+    schema.save(save_dir=save_dir.joinpath("schema"), category="dataset_description")
 
 
 
