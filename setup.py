bbc351af3ebec96bfe8c3ddd65b4a56a77bedf21
Chinchien Lin <lin810116@gmail.com>
2022-08-09 10:05:50 +0800

    Add static data to release

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="sparc_me",
-    version="0.0.2",
+    version="1.0.10",
     author="Thiranja Prasad Babarenda Gamage, Chinchien Lin, Savindi Wijenayaka, Michael Hoffman, Linkun Gao, Haribalan Kumar",
     email="psam012@aucklanduni.ac.nz, clin864@aucklanduni.ac.nz",
     license="Apache-2.0",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['resources./*']},
     install_requires=[
         'pandas',
         'styleframe',
