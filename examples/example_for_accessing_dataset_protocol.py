4b995cba683470cc357b2caf5db8bf54f69a9dbd
LinkunGao <gaolinkun123@gmail.com>
2022-08-09 13:12:03 +1200

    update docstrings in example access protocol

--- examples/example_for_accessing_dataset_protocol.py
+++ examples/example_for_accessing_dataset_protocol.py
@@ -1,5 +1,8 @@
 """Example for access protocol through SPARC dataset and download protocol data as json format.
 
+This example shows how users can call the sparc-me API to get the protocol DOI from
+a curated SPARC dataset stored on Pennsieve, and then store the data as `JSON` format locally.
+
 Dataset 273 described on the SPARC Portal
 (https://sparc.science/datasets/273?type=dataset) is used as an example.
 """
