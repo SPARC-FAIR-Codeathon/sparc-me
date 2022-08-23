82d2c21de6280596f93070537a1441eba1ca5c5f
LinkunGao <gaolinkun123@gmail.com>
2022-08-24 00:41:39 +1200

    change the example print sentence

--- examples/example_for_download_whole_dataset.py
+++ examples/example_for_download_whole_dataset.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     api_tools.download_dataset(156)
     end = time.time()
 
-    print("single thread to download dataset 156 cost:", end - start, "seconds!")
+    print("multi-threads to download dataset 156 cost:", end - start, "seconds!")
