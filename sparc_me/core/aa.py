0d20025b157128e566aa053344d0bb387eaa0457
skycoco <skycoco1994@gmail.com>
2023-09-20 11:37:28 +1200

    update doc

--- sparc_me/core/aa.py
+++ sparc_me/core/aa.py
@@ -1,22 +1,7 @@
-"""aa模块的文档注释"""
+"""Metadata模块的文档注释"""
 
 
-class Aa(object):
-    """
-        Aa类的注释
-    """
-
-    @staticmethod
-    def aa_api(x, y):
-        """
-        求商
-        :param x: 整数
-        :param y: 不能为零的整数
-        :return: 两数之商
-        """
-        return x / y
-
-class Metadata(object):
+class Metadata:
     """
         metadata类的注释
     """
