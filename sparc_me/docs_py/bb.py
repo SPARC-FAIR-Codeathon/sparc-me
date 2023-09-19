d6789783b687ed44d00fbafae01a7c90cf501085
skycoco <skycoco1994@gmail.com>
2023-09-20 11:48:16 +1200

    update doc

--- sparc_me/docs_py/bb.py
+++ sparc_me/docs_py/bb.py
@@ -1,3 +1,21 @@
+
+"""aa模块的文档注释"""
+
+
+class Aa(object):
+    """
+        Aa类的注释
+    """
+
+    @staticmethod
+    def aa_api(x, y):
+        """
+        求商
+        :param x: 整数
+        :param y: 不能为零的整数
+        :return: 两数之商
+        """
+        return x / y
 class Metadata:
     def __init__(self, metadata_file, metadata, version, dataset_path):
         """
