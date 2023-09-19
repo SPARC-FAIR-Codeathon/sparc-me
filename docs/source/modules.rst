91de0ac7f42466c329c93ffb96120fddd7bab347
skycoco <skycoco1994@gmail.com>
2023-09-20 11:10:40 +1200

    update doc

--- docs/source/modules.rst
+++ docs/source/modules.rst
@@ -9,3 +9,4 @@ sparc_me
    schema
    utils
    api_tools
+   aa
---
+++ sparc_me/core/aa.py
@@ -0,0 +1,17 @@
+"""aa模块的文档注释"""
+
+
+class Aa(object):
+    """
+        Aa类的注释
+    """
+
+    @staticmethod
+    def aa_api(x, y):
+        """
+        求商
+        :param x: 整数
+        :param y: 不能为零的整数
+        :return: 两数之商
+        """
+        return x / y
